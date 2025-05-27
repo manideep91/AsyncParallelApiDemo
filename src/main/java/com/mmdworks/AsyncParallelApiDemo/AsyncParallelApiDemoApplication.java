c436d877bfa5055679176f17aa2649b707616a4c
Manideep Manda <mandamanideep91@gmail.com>
2025-05-27 19:46:06 -0400

    Initial commit

---
+++ src/main/java/com/mmdworks/AsyncParallelApiDemo/AsyncParallelApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mmdworks.AsyncParallelApiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncParallelApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncParallelApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmdworks/AsyncParallelApiDemo/AsyncParallelApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmdworks.AsyncParallelApiDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncParallelApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
